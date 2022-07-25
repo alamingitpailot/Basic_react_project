b7b9968cacb85d33a0b94a55081b2a5d20f73a48
shahinurcode <Sr_srabonbd@yahoo.com>
2022-07-25 06:25:33 +0600

    Simple useState use button value increment

--- App.js
+++ App.js
@@ -1,41 +1,12 @@
- 
-import './App.css';
+import React from 'react'
+import UseState from './components/UseState'
 
-import React, { Component } from 'react'
-import Products from './components/Products'
-import axios from 'axios';
- 
-class App extends Component {
+export default function App() {
+  return (
+    <div>
+      <UseState />
 
-  state = {
-    products: [],
-    ps:false
-  }
-  
-  productShow = (e) => {
-    this.setState({ ps: true });
-  }
-
-  componentDidMount() {
-    axios.get('https://jsonplaceholder.typicode.com/todos').then(
-      res => this.setState({products:res.data})
-    )
-  }
-
-  render() {
-
-    var productMarkup = '';
-    if (this.state.ps === true) {
-      productMarkup = <Products items={ this.state.products } />
-    }
-
-     return (
-        <div> 
-         <button onClick={this.productShow}>Show Product</button>
-         { productMarkup }
-        </div>
-     )
-   }
+    </div>
+  )
 }
-export default App;
- 
+
